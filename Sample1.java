286a6c1fa37036f0ce8644c5ddd2876d19c00406
RahulWagh369 <164021258+RahulWagh369@users.noreply.github.com>
2024-03-22 10:34:24 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,26 @@
+package Variables;
+
+public class Sample1 {
+	
+	public static void main(String[] args) {
+		
+		// variable declaration
+		
+		String Fristname;
+		int    Rollnumber;
+		
+		// variable inicialization
+		
+		Fristname = "rahul wagh";
+		Rollnumber = 369;
+		
+		//usage
+		
+		System.out.println("Frist name --->"+Fristname);
+		System.out.println("Rollnumber --->"+Rollnumber);
+		
+		
+		
+	}
+
+}
